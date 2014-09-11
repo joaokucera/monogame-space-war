bc1c83e8cb47857ea03b8052aab0ca472c2c4e9c
joaokucera <falleirok@gmail.com>
2014-09-11 17:23:32 -0300

    create project
    create project

---
+++ README.md
@@ -0,0 +1,19 @@
+A shoot'em up game made with monogame
+=====================================
+
+The proposal here is to create a shoot'em up game with monogame.
+
+Compatility
+===========
+
+Windows.
+
+License
+=======
+
+Licensed under the MIT License.
+
+Change Log
+==========
+
+0.0.1 First version.
---
+++ source/Spacewar/Spacewar.cs
@@ -0,0 +1,51 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace Spacewar
+{
+    public class Spacewar : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Spacewar()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
